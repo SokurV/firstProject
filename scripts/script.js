f66106774a1fd906afbf7a4ceef26dbf025c1d6e
SokurV <windows607@gmail.com>
2022-09-12 15:08:20 +0600

    Написал свой JS-код для работы меню и смуз скролла

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,28 @@
-header__burger_button.onclick = function myFunction() {
-    let x = document.getElementById('menu');
+let buttonMenu = document.getElementById('header__burger_button')
+let blockMenu = document.getElementById('menu')
 
-    if (x.className === "container__menu") {
-        x.className += " responsive_menu";
-    } else {
-        x.className = "container__menu"
-    }
+buttonMenu.addEventListener('click', ()=>{
+    blockMenu.classList.toggle('responsive_menu')
+
+})
+
+function scrollTo(elem) {
+    window.scroll({
+        left: 0,
+        top: elem.offsetTop - 60,
+        behavior: 'smooth'
+    })
 }
+
+blockMenu.addEventListener('click', (evt)=>{
+    evt.preventDefault()
+    if(evt.target.classList.contains('menu')) {
+        blockMenu.classList.remove('responsive_menu')
+    }
+
+    let targetBlock = document.getElementById(evt.target.textContent.toLowerCase())
+    scrollTo(targetBlock) 
+
+})
+
 
