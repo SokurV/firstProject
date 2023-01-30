423dad4252dd929779e08211d85c2b40578c0ef1
Vadim <88965941+SokurV@users.noreply.github.com>
2023-01-30 21:35:28 +0600

    Отменил стандартное поведение кнопки submit

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
 let buttonMenu = document.getElementById('header__burger_button')
 let blockMenu = document.getElementById('menu')
+let submitButton = document.getElementById('submit')
 
 buttonMenu.addEventListener('click', ()=>{
     blockMenu.classList.toggle('responsive_menu')
 
+})
+
+submitButton.addEventListener('click', (event)=>{
+    event.preventDefault()
+    alert('Форма отправлена.')
 })
 
 function scrollTo(elem) {
